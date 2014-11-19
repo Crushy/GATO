55f54e39b3f19a05ce77831a51c29daa3d0cd346
Crushy <crushyerbones@gmail.com>
2014-11-19 04:51:53 +0000

    Now reads tasks from YAML file

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ class SystemTrayIcon(QSystemTrayIcon):
     GATO - Gamedev Auxiliary TOol
     Mostly sits on your taskbar, wasting a few resources and looking pretty. Occasionally it may turn out to be useful (like it's namesake)
     """
-    def __init__(self, icon, parent=None):
+
+    def __init__(self, icon, tasks, parent=None):
         QSystemTrayIcon.__init__(self, icon, parent)
         menu = QMenu(parent)
         menu.setContextMenuPolicy(Qt.ActionsContextMenu)
 
-        task1_action = QAction("Task 1", self)
-        task1_action.triggered.connect(self.execute_task)
+        for task in tasks:
+            task1_action = QAction(task.name, self)
+            task1_action.triggered.connect(self.execute_task)
 
         #Quit
         icon_quit = QIcon("process-stop.xpm")
@@ -42,22 +44,43 @@ class SystemTrayIcon(QSystemTrayIcon):
         #TODO: this quits
         msg_box.exec_()
 
-def main():
 
+# TODO mark this as a safe object
+class Task:
+    def __init__(self, name, command):
+        self.name = name
+        self.command = command
+
+    def __repr__(self):
+        return "Task: \nname: " + self.name + "\ncommand(s): " + self.command
+
+
+def main():
+    config_file = None
     try:
-        #config_stream = open("config.yaml")
-        #yaml.load(config_stream, Loader=yaml.CLoader)
+        config_stream = open("config.yaml")
         print("Loaded config file")
     except FileNotFoundError as e:
         print("Couldn't load config file: {0}".format(e.strerror))
         sys.exit(-1)
 
+    config_file = yaml.load(config_stream)
+
+    tasks = config_file[0:]
+    for task in config_file:
+        # print("-  ")
+        print(task)
+        print("\n")
+    # print( yaml.dump(config_file) )
+
+    # sys.exit(0)
     app = QApplication(sys.argv)
 
     #app.setAttribute(Qt.AA_DontShowIconsInMenus, False)
 
     w = QWidget()
-    tray_icon = SystemTrayIcon(QIcon("logo.xpm"), w)
+    print(tasks)
+    tray_icon = SystemTrayIcon(QIcon("logo.xpm"), tasks, w)
 
     tray_icon.show()
 
